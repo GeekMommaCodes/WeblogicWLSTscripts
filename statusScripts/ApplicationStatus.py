a97ffee485c562c5052053b91f3138e813c38ec8
Alex Shpak <3dleaf@mail.ru>
2012-05-12 14:25:05 +0400

    Add folders with one status script

---
+++ statusScripts/ApplicationStatus.py
@@ -0,0 +1,12 @@
+connect('whisper.wasp','whisper.wasp','t3://localhost:7001')
+domainRuntime()
+cd('AppRuntimeStateRuntime/AppRuntimeStateRuntime')
+#cd('ServerRuntimes')
+#ls()
+AppList = cmo.getApplicationIds()
+print '####### Application #######  Application State\n'
+print '***********************************************\n'
+for App in AppList:
+    print '#######',App ,' #######', cmo.getIntendedState(App)
+
+print '***********************************************\n'
